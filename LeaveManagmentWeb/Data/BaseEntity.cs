bb7277614eddf65698b34451dd5be582bdf5f08d
albert <codewithalbert@gmail.com>
2023-03-13 07:21:54 +0200

    Added LeaveType and LeaveAllocation Migrations

---
+++ LeaveManagmentWeb/Data/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagmentWeb.Data
+{
+    public partial class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
---
+++ LeaveManagmentWeb/Data/LeaveType.cs
@@ -0,0 +1,10 @@
+﻿namespace LeaveManagmentWeb.Data
+{
+    public class LeaveType: BaseEntity
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+
+    }
+}
